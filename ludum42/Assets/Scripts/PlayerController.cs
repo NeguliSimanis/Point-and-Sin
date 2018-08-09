91ab32d42e7c01d96793aeccd73b3079520f5633
NeguliSimanis <simanism@inbox.lv>
2018-08-09 23:38:19 +0300

    Added basic movement
    sorry for no comment code

---
+++ ludum42/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    #region DATA
+    [SerializeField] private float moveSpeed;
+    #endregion
+
+    #region MOVEMENT
+    private Vector2 targetPosition;
+    private Vector2 dirNormalized;
+    float timeToReachTarget;
+    #endregion
+
+    #region COMPONENTS
+    Rigidbody2D rigidBody2D;
+    #endregion
+
+    void Start()
+    {
+        rigidBody2D = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetTargetPositionAndDirection();
+        }
+        if (Vector2.Distance(targetPosition, transform.position) <= 0.01f)
+        {
+            return; 
+        }
+        else
+        {
+            MovePlayer(); 
+        }
+    }
+
+    void GetTargetPositionAndDirection()
+    {
+        targetPosition = Input.mousePosition;
+        targetPosition = Camera.main.ScreenToWorldPoint(targetPosition);
+        dirNormalized = new Vector2(targetPosition.x - transform.position.x, targetPosition.y - transform.position.y);
+        dirNormalized = dirNormalized.normalized;
+    }
+
+    void MovePlayer()
+    {
+        transform.position = new Vector2(transform.position.x, transform.position.y) + dirNormalized * moveSpeed * Time.deltaTime;
+    }
+}
