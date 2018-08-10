503d5bf1a39a21c2390d379515b885b1086e42b1
NeguliSimanis <simanism@inbox.lv>
2018-08-10 22:03:39 +0300

    Enemy follows player if near him

---
+++ ludum42/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    #region DATA variables
+    float moveSpeed = 0.13f;
+    [SerializeField] float sightRadius = 3f; // if player moves closer than this, he will be noticed
+    #endregion
+
+    #region STATE variables
+    bool isPlayerVisible = false; // if true, move towards player to attack. If false, patrol the area
+    bool isNearPlayer = false; // if true, stop to attack the player
+    #endregion
+
+    #region MOVEMENT variables
+    private Vector2 targetPosition;
+    private Vector2 dirNormalized;
+    private Transform targetTransform;
+    #endregion
+
+    #region OTHER variables
+    [SerializeField] Transform playerTransform;
+    #endregion
+
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        CheckIfPlayerVisible();
+        if (!isPlayerVisible)
+        {
+            PatrolArea();
+        }
+        else
+        {
+            FollowPlayer();
+        }
+	}
+
+    void CheckIfPlayerVisible()
+    {
+        if (Vector2.Distance(playerTransform.position, transform.position) <= sightRadius)
+        {
+            isPlayerVisible = true;
+            Debug.Log("near!");
+        }
+        else
+        {
+            isPlayerVisible = false;
+            Debug.Log("far!");
+        }
+    }
+
+    void PatrolArea()
+    {
+
+    }
+
+    void FollowPlayer()
+    {
+        targetPosition = playerTransform.position;
+        GetTargetPositionAndDirection();
+        MoveEnemy();
+    }
+
+    void GetTargetPositionAndDirection()
+    {
+        dirNormalized = new Vector2(targetPosition.x - transform.position.x, targetPosition.y - transform.position.y);
+        dirNormalized = dirNormalized.normalized;
+    }
+
+    void MoveEnemy()
+    {
+        transform.position = new Vector2(transform.position.x, transform.position.y) + dirNormalized * moveSpeed * Time.deltaTime;
+    }
+}
