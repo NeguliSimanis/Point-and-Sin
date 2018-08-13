12adbc938ed3dc353ab24a7f971f90d9261e8229
NeguliSimanis <simanism@inbox.lv>
2018-08-13 20:03:04 +0300

    added lava sounds

---
+++ ludum42/Assets/Scripts/EnableLavaSFX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableLavaSFX : MonoBehaviour
+{
+
+
+    [SerializeField]
+    LavaSounds lavaSounds;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            lavaSounds.EnableLavaSFX();
+        }
+    }
+
+}
