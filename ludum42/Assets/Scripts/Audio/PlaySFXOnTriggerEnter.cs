f81303476fe13a553356fcbba199a2475221297a
NeguliSimanis <simanism@inbox.lv>
2019-03-31 21:46:06 +0300

    0.16.5 - added lava fall sfx
    - deleted some unused assets

---
+++ ludum42/Assets/Scripts/Audio/PlaySFXOnTriggerEnter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySFXOnTriggerEnter : MonoBehaviour
+{
+    [SerializeField]
+    AudioClip SFXtoPlay;
+    [SerializeField]
+    float SFXVolume;
+    bool hasPlayedSFX = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (hasPlayedSFX)
+            return;
+        if (collision.gameObject.tag == "Player")
+        {
+            hasPlayedSFX = true;
+            gameObject.GetComponent<AudioSource>().PlayOneShot(SFXtoPlay, SFXVolume);
+        }
+    }
+
+
+}
+
+
--- ludum42/Assets/Scripts/LoadLevel.cs
+++ ludum42/Assets/Scripts/LoadLevel.cs
@@ -8,6 +8,6 @@ public class LoadLevel : MonoBehaviour
     public void Load()
     {
         PlayerData.current.Reset();
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("Main");
     }
 }
