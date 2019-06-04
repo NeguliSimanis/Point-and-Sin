bf96ff1fb7c04c99413f06f32983f3a91eb7a583
NeguliSimanis <simanism@inbox.lv>
2019-06-05 00:50:03 +0300

    0.16.6 - itch io build

--- ludum42/Assets/Scripts/GameManual.cs
+++ ludum42/Assets/Scripts/GameManual.cs
@@ -8,7 +8,7 @@ public class GameManual : MonoBehaviour
     [SerializeField]
     Text helpText;
 
-    string gameplayHelpText = "Kill enemies to level up. With each new level, you gain 2 sin points, which can be spent to make you stronger.\n\nEnemies drop items. Equip items to get stronger.\n\nUnlock brutal mode by defeating the boss and finding the vial of virgin blood.";
+    string gameplayHelpText = "Kill enemies to level up. With each new level, you gain 2 sin points, which can be spent to make you stronger.\n\nEnemies drop items. Equip items to get stronger. Eat items to recover life or mana.\n\nUnlock brutal mode by defeating the boss and finding the vial of virgin blood.";
     string controlsHelpText = "Right click  -  use active ability\nLeft click  -  walk, pick up items\nMouse wheel  –  switch active ability\nC  -  open character panel\nI  -  open inventory\nEsc  -  pause game";
     string sinsHelpText = "Wrath - increases melee damage, spell damage; increases spell cost\n\nPride - increases health and mana\n\nLust - increases mana regeneration, melee critical chance, reduces spell cooldown";
 
