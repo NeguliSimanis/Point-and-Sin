26915688bf073b8cdec2d08278d728f74fb39fe6
NeguliSimanis <simanism@inbox.lv>
2019-08-03 02:31:03 +0300

    yay, shadows and shit seem to work

---
+++ ludum42/Assets/Scripts/AnimationEventManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Contains methods that are called from animation frames
+/// </summary>
+public class AnimationEventManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] characterShadows;
+    int previousShadowID = -1;
+   
+
+    public void SetCharacterShadows(int activeShadowID)
+    {
+        if (characterShadows.Length == 0)
+        {
+            Debug.Log("char shadows not set!");
+            return;
+        }
+
+        characterShadows[activeShadowID].SetActive(true);
+        if (previousShadowID != -1 && previousShadowID != activeShadowID)
+        {
+            characterShadows[previousShadowID].SetActive(false);
+        }
+        previousShadowID = activeShadowID;
+    }
+}
--- ludum42/Assets/Scripts/FadeOut.cs
+++ ludum42/Assets/Scripts/FadeOut.cs
@@ -4,21 +4,68 @@ using UnityEngine;
 using UnityEngine.UI;
 
 /// <summary>
+/// Fades out selected UI image, text, or sprite upon enabling
+/// 
+/// Some noted usage:
+///     - fading out enemy shadows upon death
+/// 
 /// Code taken from here: https://www.youtube.com/watch?v=oNz4I0RfsEg
 /// </summary>
 public class FadeOut : MonoBehaviour
 {
     [SerializeField]
+    bool fadeOutOnEnable = true;
+    [SerializeField]
     Image imageToFadeOut;
     [SerializeField]
     Text textToFadeOut;
+    [SerializeField]
+    SpriteRenderer[] spritesToFadeOut;
+    SpriteRenderer spriteToFadeOut;
 
     float fadeSpeed = 0.015f;
+    [SerializeField]
+    float spriteFadeSpeed = 0.2f;
+    [SerializeField]
+    float spriteFadeInterval = 1f;
 
     private void OnEnable()
     {
-        StartCoroutine("FadeOutImage");
-        StartCoroutine("FadeOutText");
+        if (fadeOutOnEnable)
+        {
+            StartCoroutine("FadeOutImage");
+            StartCoroutine("FadeOutText");
+        }
+        
+    }
+
+    public void FadeSprite()
+    {
+        // find which of the given sprites is active and fade it out
+        for (int i = 0; i < spritesToFadeOut.Length; i++)
+        {
+            if(spritesToFadeOut[i].gameObject.activeInHierarchy)
+            {
+                Debug.Log("sprite found!");
+                spriteToFadeOut = spritesToFadeOut[i];
+            }
+        }
+        Debug.Log("called!");
+        StartCoroutine("FadeOutSprite");
+    }
+    
+    IEnumerator FadeOutSprite()
+    {
+        Debug.Log("corotine1!");
+        yield return new WaitForSeconds(spriteFadeInterval);
+        Debug.Log("corotine!");
+        for (float f = 1f; f >= -0.05f; f -= spriteFadeSpeed)
+        {
+            Color c = spriteToFadeOut.color;
+            c.a = f;
+            spriteToFadeOut.color = c;
+            yield return new WaitForSeconds(0.07f);
+        }
     }
 
     IEnumerator FadeOutText()
